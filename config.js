1c063d1beca0dcbeacfb45de5ad044bb54bc3ded
lvtianxinyuan <32013546+lvtianxinyuan@users.noreply.github.com>
2019-02-06 04:05:05 +0800

    添加接口日志，修改自己注册获取的配置文件

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   version: "5.1",
   note: '优化接口调用流程',
-  subDomain: "tz", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
-  appid: "wxa46b09d413fbcaff", // 您的小程序的appid，购物单功能需要使用
+  subDomain: "lvtian", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
+  appid: "wx32b5ac708ae7dc01", // 您的小程序的appid，购物单功能需要使用
   shareProfile: '百款精品商品，总有一款适合您' // 首页转发的时候话术
 }
 /*
